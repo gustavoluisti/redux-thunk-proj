56fac1a64f56d84a20669284ef95f9cf323c3dda
LUIS GUSTAVO SANTOS <LUSANTOS@bbmapfre.com.br>
2018-12-18 15:57:55 -0200

    Enviando arquivos

---
+++ src/Info.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { loadData } from './actions'
+
+export class Info extends Component{
+    componentDidMount() {
+       this.props.loadData()
+    }
+    render(){   
+        if(this.props.isFetching){
+            return <span>Loading...</span>
+        }
+        return <span>Info: {this.props.data.origin}</span>
+        {JSON.stringify(this.state.data.name)}
+    }
+    
+}
+const mapStateToProps = (state) => {
+    return{
+        isFetching: state.isFetching,
+        data: state.data
+    }    
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        loadData: () => dispatch(loadData())
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Info)
---
+++ src/reducer.js
@@ -0,0 +1,19 @@
+const INITIAL_STATE = {
+    data: [],
+    isFetching: false
+  }
+  
+  export const reducer = (state = INITIAL_STATE, action) => {
+    if (action.type === 'LOAD_DATA_REQUEST'){
+      return {
+        isFetching: true,
+        data: []
+      }
+    }else if(action.type === 'LOAD_DATA_SUCCESS'){
+      return{
+        isFetching: false,
+        data: action.data
+      }
+    }
+    return state
+  }
